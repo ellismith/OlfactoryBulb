13b8daabfcce885adbb7fb62d4e2d547d4757988
Elliot Smith <smith.elliot.ann@gmail.com>
2025-03-18 09:46:08 -0700

    added xmin and xmax to plot lfp across all electrodes

--- notebooks/multichannel_analysis.py
+++ notebooks/multichannel_analysis.py
@@ -7,30 +7,28 @@ try:
 except:
     import pickle as cPickle
 
-from lfp_analysis_current import get_dirs
+import yaml
+
+from load import get_dirs
 
 
-def show_multichannel_lfp(paramset, params_short=True):
+def plot_multichannel_lfp(paramset, xmin=None, xmax=None, params_short=True):
     """
     Loads LFP data from pkl files and plots the traces for all electrode locations.
-    e.g. show_multichannel_lfp("Multi_10chan_200apart")
+    e.g. plot_multichannel_lfp("Multi_10chan_200apart")
 
     :param paramset: The parameter set used for the simulation.
+    :param xmin: Minimum x-limit for the plot. Defaults to first value in t_lfp.
+    :param xmax: Maximum x-limit for the plot. Defaults to last value in t_lfp.
     :param params_short: Boolean indicating if the parameter set name should be shortened.
     """
     results_dir, paramset_dir, fig_dir = get_dirs(paramset)
-
-    # Update results_dir to include paramset
     results_dir = os.path.join(results_dir, paramset)
-
     fig_width = 27
-
-
 
     with open(os.path.join(paramset_dir, 'params.yml'), 'rb') as f:
         params_dict = yaml.load(f, Loader=yaml.FullLoader)
 
-    
     probe_n_electrodes = params_dict['probe_n_electrodes']
     probe_spacing = params_dict['probe_spacing']
     x = params_dict['probe_x']
@@ -53,12 +51,12 @@ def show_multichannel_lfp(paramset, params_short=True):
             t_lfp, lfp = cPickle.load(f)
 
         ax[i].plot(t_lfp, lfp)
-        ax[i].set_title(f'Electrode at {location}', fontsize=18)
+        ax[i].set_title(f'Electrode at {location}, from {xmin} to {xmax} ms', fontsize=18)
+        ax[i].set_xlim(xmin if xmin is not None else t_lfp[0],
+                        xmax if xmax is not None else t_lfp[-1])
+        ax[i].set_xticks(np.arange(xmin if xmin is not None else t_lfp[0], 
+                                   xmax if xmax is not None else t_lfp[-1], 
+                                   2.0))
+        ax[i].tick_params(axis='x', labelsize=18)
         ax[i].set_xlabel('Time (ms)', fontsize=18)
         ax[i].set_ylabel('LFP (mV)', fontsize=18)
-        
-
-    #plt.title(f'LFP for vertical probe with {probe_n_electrodes} electrodes, {probe_spacing} um spacing', fontsize=16)
-    
-    plt.savefig(os.path.join(fig_dir, f'subplot{probe_n_electrodes}electrodes_{probe_spacing}spacing.jpg'), bbox_inches='tight', dpi=300)
-    plt.show()
